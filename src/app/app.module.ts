ea208b6fc34908f2f2717bc7178ded086abf6d4f
sheker03 <sheker01@gmail.com>
2020-01-16 09:22:23 +0530

    Added views and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './view/project/project.component';
+import { UserComponent } from './view/user/user.component';
+import { TaskComponent } from './view/task/task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    UserComponent,
+    TaskComponent,
+    ViewTaskComponent
   ],
   imports: [
     BrowserModule,
